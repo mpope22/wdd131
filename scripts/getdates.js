9958d11239636f289a14f0b2518c5f9a9f67d5f0
mpope22 <20mpope23@gmail.com>
2026-01-10 02:08:31 -0700

    Home page 2.0

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,4 @@
-const year = document.querySelector("#year")
+const year = document.querySelector("#year");
 document.getElementById("lastmodified").innerHTML = document.lastModified;
+// document.getElementById("year").innerHTML = (new Date(document.lastModified)).getFullYear();
+year.innerHTML = (new Date(document.lastModified)).getFullYear();
